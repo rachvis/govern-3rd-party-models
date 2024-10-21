23887cbe0fd9cdd26255057ace1b01844be90ffd
Rachana Vishwanathula <vrachana9@gmail.com>
2024-10-21 10:54:07 -0700

    Update files

--- VPC VSI Script/install.sh
+++ VPC VSI Script/install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y python3 python3-pip python3-venv
 
 
-git clone https://github.com/yourusername/repo.git
+git clone https://github.com/krishnac7/techXchange24-gov-lab
 
 
 cd repo
